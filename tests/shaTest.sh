3b4df6151ede0d62afbb1d116688916c534964cf
CJ <cj@wolfssl.com>
2014-08-12 14:51:31 -0600

    added testing features

---
+++ tests/shaTest.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+GREEN='\e[0;32m'
+RED='\e[0;31m'
+NC='\e[0m'
+ZERO=0
+fail=0
+i=0
+total=0
+ 
+function hashtest() {
+    fail=0
+    i=0
+    total=0
+    
+    while read -r md; do
+        if [[ $i -lt 10 ]]; then
+            FILE="byte000$i.dat"
+        elif [[ $i -lt 100 ]]; then
+            FILE="byte00$i.dat"
+        elif [[ $i -lt 1000 ]]; then
+            FILE="byte0$i.dat"
+        else 
+            break
+        fi   
+#takes last 3 characters off
+        md="${md:0:-$3}"
+#converts to lowercase
+        md=${md,,}
+        cipher="$(../programs/cyassl hash -$2 -i $FILE -l $i)"
+        if test "$cipher" != "$md"; then
+            fail=$[fail+1]
+        fi
+        total=$[total+1]
+        i=$[i+1]
+    done < $1
+    
+    if [ $fail = $ZERO ]; then
+        echo -e "${GREEN}All $total $2 Tests Passed${NC}"
+    else
+        echo -e "${RED}$fail/$total $2 Tests Failed${NC}"
+    fi
+}
+
+echo Testing...
+hashtest byte-hashes.sha1 sha 3
+hashtest byte-hashes.md5 md5 2
